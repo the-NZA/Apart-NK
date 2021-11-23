77c47a2f99cf74b750c11be6fec3ced757d07e44
the_nza <the_nza@protonmail.com>
2021-11-23 19:09:51 +0300

    Add more styles for homepage: herosection, header, shadows, etc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // import tester from "./js/tester.js";
 // import HandleShowCartButton from "./js/showcart.js";
 
-import "normalize.css";
-import "milligram";
+import "normalize.css"; // Import normalize
+import "milligram"; // Import miligram
 import "./css/index.css"; // Import styles
 
 window.addEventListener("DOMContentLoaded", function () {
