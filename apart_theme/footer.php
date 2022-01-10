8f597c565862108061649438ad58ea90b689e2e3
the_nza <the_nza@protonmail.com>
2022-01-10 18:53:45 +0300

    Add booking link to footer

--- apart_theme/footer.php
+++ apart_theme/footer.php
@@ -1,58 +1,65 @@
 <?php
+
 /**
  * The template for displaying the footer.
  * Contains the closing of the tags and <footer>
  */
 ?>
-	<div class="site-go-booking">
-		<a href="#">Перейти к бронированию</a>
-	</div>
+<div class="site-go-booking">
 
-	<footer class="site-footer">
-		<div class="footer wrapper">
-			<div class="footer__widgets">
-				<div class="footerwidget">
-					<h3 class="footerwidget__title">О нас</h3>
-					<p class="footerwidget__text">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-						fugiat veritatis veniam dolorum? Suscipit commodi blanditiis culpa harum libero dolorum, id
-						dicta ipsum ex!
-					</p>
-				</div>
+	<a href="
+	<?php
+	// 26 – ID for booking page
+	echo get_page_link(26);
+	?>">Перейти к бронированию</a>
+</div>
 
-				<div class="footerwidget">
-					<h3 class="footerwidget__title">Контакты</h3>
-					<ul class="footerwidget__body">
-						<li class="footerwidget__grid"><span>Телефон:</span><a href="#">89998887766</a></li>
-						<li class="footerwidget__grid"><span>Email:</span><a href="#">test@test.ru</a></li>
-					</ul>
-				</div>
-
-				<div class="footerwidget">
-					<h3 class="footerwidget__title">Меню</h3>
-					<ul class="footerwidget__body">
-						<li><a href="#">Главная</a></li>
-						<li><a href="#">Апартаменты</a></li>
-						<li><a href="#">Забронировать</a></li>
-						<li><a href="#">Контакты</a></li>
-						<li><a href="#">Услуги</a></li>
-					</ul>
-				</div>
-
-				<div class="footerwidget">
-					<h3 class="footerwidget__title">Apark-NK</h3>
-					<p class="footerwidget__text">
-						г. Москва, Пресненская набережная д.12
-					</p>
-				</div>
+<footer class="site-footer">
+	<div class="footer wrapper">
+		<div class="footer__widgets">
+			<div class="footerwidget">
+				<h3 class="footerwidget__title">О нас</h3>
+				<p class="footerwidget__text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+					fugiat veritatis veniam dolorum? Suscipit commodi blanditiis culpa harum libero dolorum, id
+					dicta ipsum ex!
+				</p>
 			</div>
 
-			<div class="footer__copyright">
-				2021 &copy; <a href="#">Apark-NK</a>
+			<div class="footerwidget">
+				<h3 class="footerwidget__title">Контакты</h3>
+				<ul class="footerwidget__body">
+					<li class="footerwidget__grid"><span>Телефон:</span><a href="#">89998887766</a></li>
+					<li class="footerwidget__grid"><span>Email:</span><a href="#">test@test.ru</a></li>
+				</ul>
+			</div>
+
+			<div class="footerwidget">
+				<h3 class="footerwidget__title">Меню</h3>
+				<ul class="footerwidget__body">
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">Апартаменты</a></li>
+					<li><a href="#">Забронировать</a></li>
+					<li><a href="#">Контакты</a></li>
+					<li><a href="#">Услуги</a></li>
+				</ul>
+			</div>
+
+			<div class="footerwidget">
+				<h3 class="footerwidget__title">Apark-NK</h3>
+				<p class="footerwidget__text">
+					г. Москва, Пресненская набережная д.12
+				</p>
 			</div>
 		</div>
-	</footer>
 
-	<?php wp_footer(); ?>
+		<div class="footer__copyright">
+			2021 &copy; <a href="#">Apark-NK</a>
+		</div>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
 </body>
+
 </html>
