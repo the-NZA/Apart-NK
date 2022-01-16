f9dc77fa06d7fb2678b943c5812ae73e247bb41c
the_nza <the_nza@protonmail.com>
2022-01-16 21:52:52 +0300

    Add slick styles and settings

--- src/js/slick/apartments.js
+++ src/js/slick/apartments.js
@@ -1,11 +1,14 @@
 export default function HandleApartmentsGallery() {
 	jQuery(document).ready(function ($) {
 		$('.apartgallery').slick({
-			dots: true,
 			infinite: true,
 			speed: 500,
 			slidesToShow: 1,
 			adaptiveHeight: true,
+			autoplay: true,
+			autoplaySpeed: 6000,
+			prevArrow: '<button class="slick-prev">&#10094;</button>',
+			nextArrow: '<button class="slick-next">&#10095;</button>',
 		});
 	});
 }
