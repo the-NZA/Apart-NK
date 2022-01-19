9cb50aa713cb879867e2185ff286d6d227881145
the_nza <the_nza@protonmail.com>
2022-01-19 11:48:33 +0300

    Add disabling scroll for mobile menu

--- src/js/nav/navbtn.js
+++ src/js/nav/navbtn.js
@@ -1,11 +1,13 @@
 function HandleMobileNavMenu() {
-	const header = document.querySelector('.header')
+	const siteBody = document.querySelector(".site-body");
+	const header = siteBody.querySelector('.header')
 	const headerNavbtn = header.querySelector(".header__navbtn")
 	const headerNav = header.querySelector(".header__nav")
 
 	headerNavbtn.addEventListener("click", function (e) {
 		e.preventDefault()
 
+		siteBody.classList.toggle("site-body--no-scroll")
 		headerNavbtn.classList.toggle("navbtn--active")
 		headerNav.classList.toggle("header__nav--active")
 	})
